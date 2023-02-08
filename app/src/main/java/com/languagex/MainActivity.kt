a227e54e3af0243917cefa0950185f738cdfa919
zhaojian <zhaojian@apusapps.com>
2023-02-08 18:32:39 +0800

    第一次提交

---
+++ app/src/main/java/com/languagex/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.languagex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val name = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val arrayListOf = arrayListOf<MainActivity>()
+    }
+}
